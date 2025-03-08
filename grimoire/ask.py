471a0cd381ed8a426f732203f1b4400a58259520
JohnTrunix <61423865+JohnTrunix@users.noreply.github.com>
2025-03-08 16:45:29 +0100

    refactor(ask): make use of langchain llm client
    this enables dynamic client usage in the future

--- grimoire/ask.py
+++ grimoire/ask.py
@@ -1,30 +1,39 @@
 import os
 
 import typer
-from google import genai
+from langchain.chat_models import init_chat_model
+from langchain.chat_models.base import BaseChatModel
 
 ask_cli = typer.Typer()
 
 
-def verify_api_key() -> None:
+def get_llm_client() -> BaseChatModel:
+    """
+    Configures a chat model client with the api key
+
+    :return: defined chat model client
+    """
     if not os.getenv("LLM_API_KEY"):
         typer.echo("LLM_API_KEY environment variable is not set.")
         raise typer.Exit(code=1)
 
+    # TODO: add configuration options for other models and options for model temperature, etc.
+    return init_chat_model(
+        model_provider="google_genai",
+        model="gemini-2.0-flash",
+        api_key=os.getenv("LLM_API_KEY"),
+        temperature=0,
+    )
+
 
 @ask_cli.command("ask", help="Ask a question with project context")
 def ask(question: list[str]) -> None:
-    verify_api_key()
-
     question_prefix = typer.style("Question", fg=typer.colors.BLUE, bold=True)
     answer_prefix = typer.style("Answer", fg=typer.colors.RED, bold=True)
     typer.echo(f"{question_prefix}: {' '.join(question)}")
 
-    client = genai.Client(api_key=os.getenv("LLM_API_KEY"))
-    response = client.models.generate_content_stream(
-        model="gemini-2.0-flash",
-        contents=question,
-    )
+    client = get_llm_client()
+    response = client.stream(" ".join(question))
     typer.echo(f"{answer_prefix}: ", nl=False)
     for chunk in response:
-        typer.echo(chunk.text, nl=False)
+        typer.echo(chunk.content, nl=False)
