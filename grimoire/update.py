2a38b4d0e4f3beb4d5bf640946a41e9b4af6ccd0
JohnTrunix <61423865+JohnTrunix@users.noreply.github.com>
2025-04-14 21:26:22 +0200

    feat(cli): init update command

---
+++ grimoire/update.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+import typer
+
+from grimoire.configuration import CONFIG_FILE_NAME, ProjectConfiguration
+from grimoire.init import DUMMY_SOURCES
+
+update_cli = typer.Typer()
+
+
+@update_cli.command(
+    "update", help="Update the grimoire sources with the latest dependencies"
+)
+def update(
+    path: Path = typer.Argument(  # noqa: B008
+        Path.cwd(),  # noqa: B008
+        help="Path to the grimoire project",
+    ),
+) -> None:
+    config = ProjectConfiguration.load_from_yaml(path / CONFIG_FILE_NAME)
+
+    config.sources = []
+
+    # TODO: add sources from dep files
+    for source in DUMMY_SOURCES:
+        config.sources.append(source)
+
+    config.save_to_yaml(path / CONFIG_FILE_NAME)
